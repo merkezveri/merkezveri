56b6f4a657af221f0d3efdb673c30350d97e01ff
Furkan Tolga Yüce <yucefurkantolga@gmail.com>
2021-02-23 02:55:52 +0300

    sidebarItemList moved js file

--- src/components/SidebarMenu.js
+++ src/components/SidebarMenu.js
@@ -1,44 +1,7 @@
 import React from 'react';
 import SidebarItem from "./SidebarItem";
 
-const sidebarItemList = [
-{
-    "title" : "Üretim İstatistikleri",
-    "id" : "collapseUretim",
-    "items" : [
-        {
-        "name" : "Gayri Safi Yurtiçi Hasıla",
-        "link" : "/"
-    },
-    {
-        "name" : "Sanayi Üretim Endeksii",
-        "link" : "/"
-    },
-    {
-        "name" : "İmalat Sanayi Kapasite Kullanım Oranları",
-        "link" : "/"
-    }
-    ],
-},
-{
-    "title" : "Yatırım İstatistikleri",
-    "id" : "collapseYatırım",
-    "items" : [
-        {
-        "name" : "İnşaat İstatistikleri",
-        "link" : "/"
-    },
-    {
-        "name" : "Konut Fiyat Endeksi",
-        "link" : "/"
-    },
-    {
-        "name" : "Kurulan ve Kapanan Şirket",
-        "link" : "/"
-    }
-    ],
-}
-]
+import sidebarItemList from '../sidebarItemList';
 
 function SidebarMenu() {
     return (
---
+++ src/sidebarItemList.js
@@ -0,0 +1,40 @@
+const sidebarItemList = [
+    {
+        "title" : "Üretim İstatistikleri",
+        "id" : "collapseUretim",
+        "items" : [
+            {
+            "name" : "Gayri Safi Yurtiçi Hasıla",
+            "link" : "/"
+        },
+        {
+            "name" : "Sanayi Üretim Endeksii",
+            "link" : "/"
+        },
+        {
+            "name" : "İmalat Sanayi Kapasite Kullanım Oranları",
+            "link" : "/"
+        }
+        ],
+    },
+    {
+        "title" : "Yatırım İstatistikleri",
+        "id" : "collapseYatırım",
+        "items" : [
+            {
+            "name" : "İnşaat İstatistikleri",
+            "link" : "/"
+        },
+        {
+            "name" : "Konut Fiyat Endeksi",
+            "link" : "/"
+        },
+        {
+            "name" : "Kurulan ve Kapanan Şirket",
+            "link" : "/"
+        }
+        ],
+    }
+    ]
+
+export default sidebarItemList;
