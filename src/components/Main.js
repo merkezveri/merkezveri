19b5c97d9c6f1ba25ed9430b354bddb8d9bedd31
Furkan Tolga Yüce <yucefurkantolga@gmail.com>
2021-02-28 02:55:50 +0300

    FlourishIframe Component Oluşturuldu.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import FlourishGetImage from "./FlourishGetImage";
 import TwitterShare from "./TwitterShare";
-
-function Iframe(props) {
-    const iframe = "<iframe src='https://flo.uri.sh/visualisation/"+props.flourish_id+"/embed' title='Interactive or visual content' frameborder='0' scrolling='no' style='width:100%;height:100%;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/"+props.flourish_id+"/?utm_source=embed&utm_campaign=visualisation/"+props.flourish_id+"' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>"
-    return (<div className="Iframe" dangerouslySetInnerHTML={ {__html: iframe?iframe:""}} />);
-  }
-
+import FlourishIframe from "./FlourishIframe";
 
 function Main(props) {
 
@@ -28,7 +23,7 @@ function Main(props) {
                     <FlourishGetImage flourish_id={props.item.flourish_id} name={props.item.name} />
                     <TwitterShare name = {props.item.name}/>
                 </div>
-                <Iframe flourish_id={props.item.flourish_id} />
+                <FlourishIframe flourish_id={props.item.flourish_id} />
             </main>
         </React.Fragment>
     )
