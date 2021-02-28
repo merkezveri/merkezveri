36b9b4210f6817a2b2a2121fa082103da2cbc8e5
Furkan Tolga Yüce <yucefurkantolga@gmail.com>
2021-02-28 03:31:00 +0300

    Resim hooks kullanılarak çekiliyor ve Helmet kullanarak meta etiket oluşturuyor.

--- src/components/FlourishGetImage.js
+++ src/components/FlourishGetImage.js
@@ -1,24 +1,31 @@
-import React from 'react';
-var JSSoup = require('jssoup').default;
-
-
+import React, { useState, useEffect } from 'react';
+import {Helmet} from "react-helmet";
 
 function FlourishGetImage(props) {
-    console.log("bu:",props.name)
-    const handleClick = (flourish_id,name,e) => {
+    const [ImageLink, setImageLink] = useState("link");
+
+    // componentDidMount ve componentDidUpdate kullanımına benzer bir kullanım sunar:
+    useEffect(() => {
+
+        
         //Link Çağır
-        fetch('https://public.flourish.studio/visualisation/'+flourish_id).then(
+        fetch('https://public.flourish.studio/visualisation/'+props.flourish_id).then(
             // Yukarıdaki adresi text olarak çekiyoruz.
             (response) => response.text()
             ).then(
                 (text) => {
                     //JSSoup, Python'daki beautifulsoup benzeri bir araç
+                    var JSSoup = require('jssoup').default;
                     var soup = new JSSoup(text);
                     // çektiğimiz text üzerinde meta etiketinde arama yapıyoruz.
-                    let ImageLink = soup.findAll('meta')[10].nextElement.attrs.content
+                    //let ImageLink = soup.findAll('meta')[10].nextElement.attrs.content
+                    setImageLink(soup.findAll('meta')[10].nextElement.attrs.content)
+                    document.getElementsByTagName("META")[2].content="Your description about the page or site here to set dynamically";                    
+                }     
+            );
+    });
 
-                    //console.log(ImageLink)
-                    
+    const handleClick = (ImageLink,name,e) => {
                     // Resmi indirmek için kullandığımız kod bloğu
                     var xhr = new XMLHttpRequest();
                     xhr.open("GET", ImageLink, true);
@@ -37,14 +44,23 @@ function FlourishGetImage(props) {
                     }
                     xhr.send();
                     
-                }     
-            );
-      }
+                };
 
       return (
-        <button className="btn btn-sm btn-secondary me-1 mb-1" onClick={(e) => handleClick(props.flourish_id,props.name, e)}>
+        <React.Fragment>
+        <button className="btn btn-sm btn-secondary me-1 mb-1" onClick={(e) => handleClick(ImageLink,props.name, e)}>
             <i className="fas fa-download navbar-icon"></i> Resim indir!
         </button>
+        <Helmet>
+            <title>Merkez Veri | {props.name}</title>
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="Merkez Veri" />
+                <meta name="twitter:title" content={props.name} />
+                <meta name="twitter:description" content="Verilerin görselleştirilmesi Merkez Veri tarafından yapılmıştır." />
+                <meta name="twitter:creator" content="@merkezveri" />
+                <meta name="twitter:image:src" content={ImageLink} />
+            </Helmet>
+        </React.Fragment>
     )
 }
 
