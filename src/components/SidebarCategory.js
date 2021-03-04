05c2b4f9456f45dc64a5315322cffc85603d8bde
Furkan Tolga Yüce <yucefurkantolga@gmail.com>
2021-03-04 23:05:23 +0300

    Sidebar icon custom.

--- src/components/SidebarCategory.js
+++ src/components/SidebarCategory.js
@@ -4,11 +4,15 @@ var uniqid = require('uniqid');
 
 function SidebarCategory(props) {
     const category_id = uniqid();
+    console.log(props)
     return (
         <div>
             <a className="sidebar-a" data-bs-toggle="collapse" href={"#"+category_id} role="button" aria-expanded="false" aria-controls="collapseExample">
             <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-3 text-muted">	
-                <span>{props.category}</span>	
+            <span>
+                <i className={props.filteredItems[0].sidebarCategoryIcon} ></i>
+                <span className="ps-1">{props.category}</span>
+            </span>
                 <i className="fas fa-caret-down" ></i>
         </h6>
         </a>	
